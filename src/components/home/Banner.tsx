59288581ac4f440db07d0210b6a678c37dc2f0e6
nazim1971 <md.nazimuddinaj@gmail.com>
2025-05-11 20:47:06 +0600

    Refactor banner

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,46 +1,43 @@
 'use client';
 
 import Autoplay from 'embla-carousel-autoplay';
-
 import {
   Carousel,
   CarouselContent,
-  // CarouselNext,
-  // CarouselPrevious,
+  CarouselItem,
 } from '@/components/ui/carousel';
 
 import Image from 'next/image';
 import { useRef } from 'react';
-import banner1 from '../../assets/banner1.avif'
-import banner2 from '../../assets/banner2.jpg'
-import banner3 from '../../assets/banner3.jpg'
+import banner1 from '../../assets/banner1.avif';
+import banner2 from '../../assets/banner2.jpg';
+import banner3 from '../../assets/banner3.jpg';
 
 export function Banner() {
-  const images = [banner1,banner2,banner3];
-  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
+  const images = [banner1, banner2, banner3];
+  const plugin = useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true })
+  );
 
   return (
-    <div className="max-w-[1250px] mx-auto my-12 flex justify-center">
-      <Carousel
-        plugins={[plugin.current]}
-        className="w-full max-w-7xl"
-        // onMouseEnter={plugin.current.stop}
-        // onMouseLeave={plugin.current.reset}
-      >
-        <CarouselContent className='w-full'>
+    <div className="w-full my-12">
+      <Carousel plugins={[plugin.current]} className="w-full">
+        <CarouselContent>
           {images.map((img, idx) => (
-            <Image
-              key={idx}
-              src={img}
-              width={2400}
-              height={1000}
-              content=''
-              alt="images"
-            />
+            <CarouselItem key={idx}>
+              <div className="relative w-full h-[500px]">
+                <Image
+                  src={img}
+                  alt={`Banner ${idx + 1}`}
+                  fill
+                  className="object-cover"
+                  sizes="100vw"
+                  priority
+                />
+              </div>
+            </CarouselItem>
           ))}
         </CarouselContent>
-        {/* <CarouselPrevious />
-        <CarouselNext /> */}
       </Carousel>
     </div>
   );
